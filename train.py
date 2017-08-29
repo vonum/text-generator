7af65c89de4ff4dd21e22a3e6c3ed77a69e45009
Milan Keca <mkeca@renderedtext.com>
2017-08-29 20:16:05 +0200

    Train the model

--- train.py
+++ train.py
@@ -52,3 +52,5 @@ model.compile(loss='categorical_crossentropy', optimizer='adam')
 filepath="/output/weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
+
+model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
