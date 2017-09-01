dbc95bf35177da0e19d0d7b5f30a0d0d626d9e43
Milan Keca <mkeca@renderedtext.com>
2017-09-01 20:28:12 +0200

    Import keras modules

--- train.py
+++ train.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 
 # load ascii text and covert to lowercase
