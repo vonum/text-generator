619b85849d1339b07313d0eae8efdabf486474a2
Milan Keca <mkeca@renderedtext.com>
2017-09-01 21:43:28 +0200

    Generate text based on random seed

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 from keras.models import Sequential
 from keras.layers import Dense
@@ -37,13 +38,7 @@ for i in range(0, n_chars - seq_length, 1):
 n_patterns = len(dataX)
 print 'Total Patterns: ', n_patterns
 
-# reshape inputs for neural network
-# reshape X to be [samples, time steps, features]
 X = np.reshape(dataX, (n_patterns, seq_length, 1))
-print X[0]
-# normalize
-X = X / float(n_vocab)
-# one hot encode the output variable
 y = np_utils.to_categorical(dataY)
 
 # define the LSTM model
@@ -53,11 +48,30 @@ model.add(Dropout(0.2))
 model.add(LSTM(256))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
+
+# load weights
+model.load_weights('model.hdf5')
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-# define the checkpoint
-filepath='/output/weights-improvement-{epoch:02d}-{loss:.4f}.hdf5'
-checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
-callbacks_list = [checkpoint]
+# define oposite mapping
+int_to_char = dict((i, c) for i, c in enumerate(chars))
 
-model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
+# pick a random seed
+start = np.random.randint(0, len(dataX)-1)
+pattern = dataX[start]
+print "Seed:"
+print "\"", ''.join([int_to_char[value] for value in pattern]), "\""
+
+# generate characters
+for i in range(1000):
+  x = np.reshape(pattern, (1, len(pattern), 1))
+  x = x / float(n_vocab)
+  prediction = model.predict(x, verbose=0)
+  index = np.argmax(prediction)
+  result = int_to_char[index]
+  seq_in = [int_to_char[value] for value in pattern]
+  sys.stdout.write(result)
+  pattern.append(index)
+  pattern = pattern[1:len(pattern)]
+
+print "\nDone."
